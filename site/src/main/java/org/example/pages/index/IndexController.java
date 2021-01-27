15a4a661ac81c1b039070cb57a9359c53603dad6
alexgagnon <alex.gagnon2@canada.ca>
2021-01-26 19:09:12 -0500

    move api call to service

--- site/src/main/java/org/example/pages/index/IndexController.java
+++ site/src/main/java/org/example/pages/index/IndexController.java
@@ -1,18 +1,8 @@
 package org.example.pages.index;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpRequest;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Date;
-import java.util.List;
-
-import org.example.entities.employees.EmployeeDTO;
-import org.example.util.ApiClient;
-import org.example.util.JsonBodyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,27 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
   private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 
+  @Autowired
+  private IndexService indexService;
+
   @GetMapping("/")
   public String index(Model model) {
-    HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://localhost:9443/employees"))
-        .timeout(Duration.ofMinutes(1)).GET().build();
-
-    model.addAttribute("date", Date.from(Instant.now()));
-
-    @SuppressWarnings("unchecked")
-    Class<List<EmployeeDTO>> clazz = (Class) List.class;
-    try {
-
-      List<EmployeeDTO> employees = ApiClient.getApiClient()
-          .send(request, new JsonBodyHandler<List<EmployeeDTO>>(clazz)).body().get();
-
-      model.addAttribute("employees", employees);
-
-      return "index";
-    } catch (IOException | InterruptedException e) {
-      log.error(e.getMessage());
-    }
-
-    return null;
+    model.addAttribute("employees", indexService.getEmployees());
+    return "index";
   }
 }
