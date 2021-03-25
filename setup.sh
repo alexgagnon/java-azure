591b0ae295c1e4242f3477ca5cd1df7da0888861
alexgagnon <alex.gagnon2@canada.ca>
2021-03-25 16:34:34 -0400

    better error message

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 set -eou pipefail
 
 # install certs
-mkcert -install -pkcs12 localhost
-cp localhost.p12 ./api/src/main/resources/
-cp localhost.p12 ./site/src/main/resources/
-rm localhost.p12
+if command -v mkcert &> /dev/null; then
+  mkcert -install -pkcs12 localhost
+  cp localhost.p12 ./api/src/main/resources/
+  cp localhost.p12 ./site/src/main/resources/
+  rm localhost.p12
+else
+  echo You must install mkcert
+fi
