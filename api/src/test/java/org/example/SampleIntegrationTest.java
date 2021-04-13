7d8e9b371662615870a2aa16a8f8fc4ef2ec10fa
alexgagnon <alex.gagnon2@canada.ca>
2021-04-13 09:29:47 -0400

    remove failing integration test

--- api/src/test/java/org/example/SampleIntegrationTest.java
+++ api/src/test/java/org/example/SampleIntegrationTest.java
@@ -1,31 +1,31 @@
-package org.example;
+// package org.example;
 
-import org.springframework.context.annotation.Profile;
-import org.testcontainers.junit.jupiter.Testcontainers;
+// import org.springframework.context.annotation.Profile;
+// import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Profile("tc")
-@Testcontainers
-public class SampleIntegrationTest {
+// @Profile("tc")
+// @Testcontainers
+// public class SampleIntegrationTest {
 
-  // // container {
-  // @Container
-  // public GenericContainer<?> redis = new
-  // GenericContainer<>(DockerImageName.parse("postgres")).withExposedPorts(5432);
-  // // }
+// // container {
+// @Container
+// public GenericContainer<?> redis = new
+// GenericContainer<>(DockerImageName.parse("postgres")).withExposedPorts(5432);
+// // }
 
-  // @BeforeEach
-  // public void setUp() {
-  // String address = redis.getHost();
-  // Integer port = redis.getFirstMappedPort();
+// @BeforeEach
+// public void setUp() {
+// String address = redis.getHost();
+// Integer port = redis.getFirstMappedPort();
 
-  // System.out.println(String.format("%s:%s", address, port));
+// System.out.println(String.format("%s:%s", address, port));
 
-  // // Now we have an address and port for Redis, no matter where it is running
-  // // underTest = new RedisBackedCache(address, port);
-  // }
+// // Now we have an address and port for Redis, no matter where it is running
+// // underTest = new RedisBackedCache(address, port);
+// }
 
-  // @Test
-  // public void testSimplePutAndGet() {
-  // assertEquals("example", "example");
-  // }
-}
+// @Test
+// public void testSimplePutAndGet() {
+// assertEquals("example", "example");
+// }
+// }
